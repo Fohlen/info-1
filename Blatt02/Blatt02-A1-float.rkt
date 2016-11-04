a69bea02726409f53b8b3acc471104bfb6620af0
JuliWanner <bashslash@web.de>
2016-11-04 12:28:06 +0100

    Update Blatt02-A1-float.rkt

--- Blatt02/Blatt02-A1-float.rkt
+++ Blatt02/Blatt02-A1-float.rkt
@@ -2,13 +2,13 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt02-A1-float) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Blatt02-A1-float
-; Vergleicht ob a und b maximal um den Wert eps gleich sind
-; <bool> equal_eps(<float> a, <float> b, <float> eps)
+
+; Vergleicht ob a und b maximal um den Wert eps verschieden sind
+(: equal-eps (real real real -> boolean))
+(check-expect (equal-eps 5 2 0) #f)
+(check-expect (equal-eps 2/3 (- 1 1/3) 0) #t)
+(check-expect (equal-eps (sqrt 2) 0.7072 0) #f)
 (define equal-eps
   (lambda (a b eps)
     (<= (abs (- a b)) (abs eps))))
-    
- 
-; Zu testen:
-; 2/3 == 1 - 1/3
-(equal-eps 2/3 (- 1 1/3) 0)
+
