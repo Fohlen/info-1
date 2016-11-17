1b24f5af02dcde9ef4a20fe9b5123607b1ed01ef
Lennard Berger <lennard@berger.work>
2016-11-17 18:14:10 +0100

    update draft of A04

--- Blatt04/Blatt04-A3-calendar.rkt
+++ Blatt04/Blatt04-A3-calendar.rkt
@@ -2,10 +2,14 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt04-A3-calendar) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm")))))
 ; Ein Kalenderdatum (calendardate) besteht aus
-; Tag (number)
-; Monat (number)
-; Jahr (number)
+; - Tag (number)
+; - Monat (number)
+; - Jahr (number)
 
+(: make-calendardate (tag monat jahr -> calendardate))
+(: calendardate-tag (calendardate -> number))
+(: calendardate-monat (calendardate -> numer))
+(: calendardate-jahr (calendardate -> number))
 (define-record-procedures calendardate
   make-calendardate
   calendardate?
@@ -13,13 +17,11 @@
    calendardate-monat
    calendardate-jahr))
 
-(make-calendardate t m j ((calendardate-tag t) (calendardate-monat m) (calendardate-jahr j)))
-(calendardate-tag (number -> number))
-(calendardate-monat (number -> number))
-(calendardate-jahr (number -> number))
-
 ; Checkt ein Kalenderdatum auf Gültigkeit
 (: calendar-date-ok? (calendar-date -> boolean))
+(check-expect (calendardate-date-ok? (calendardate 10 01 1997)) #t)
 (define calendar-date-ok?
   (lambda (rc)
     (if (and (within? (calendardate-tag rc) 1 31) (within? (calendardate-monat rc) 1 12)))))
+
+; Weil es auch Jahre v.Chr gibt ist es irrelevant ob +/- und der typecheck lässt nur ganzzahlige Werte zu
