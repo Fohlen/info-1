42d90a8d3c6ee2dc131a88c52fd43259dc002966
JuliWanner <bashslash@web.de>
2016-11-25 01:44:32 +0100

    Bis auf die Shape Signatur  is jetzt eigentlich alles fertig....

--- Blatt05/A2-balance.rkt
+++ Blatt05/A2-balance.rkt
@@ -3,9 +3,13 @@
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname A2-balance) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm")))))
 (require racket/math)
 ; Wird benötigt für PI
-
+(: shape ((mixed c_circle c_rectangle c_triangle) -> shape ))
+(define shape
+  (lambda (shape)
+          shape))
+ 
+    
 ; Prefix everything with c_ for custom
-
 ; Definiert einen Kreis (circle)
 ; - radius (real)
 ; - color (string)
@@ -44,16 +48,7 @@
   (c_triangle-length
    c_triangle-color))
 
-; Definiert eine Oberfläche (shape)
-; - element (mixed)
-(: make-shape (mixed -> shape))
-(: shape-element (shape -> mixed))
-(define-record-procedures shape
-  make-shape
-  shape?
-  (shape-element))
 
-; Berechnet den Flächeninhalt eines Shapes
 (: shape-area (shape -> real))
 (define shape-area
   (lambda (s)
@@ -61,3 +56,36 @@
       [(c_circle? s) (* pi (expt (c_circle-radius) 2))]
       [(c_rectangle? s) (* (c_rectangle-height s) (c_rectangle-length s))]
       [(c_triangle? s) (* (/ (expt (c_triangle-length s) 2) 4) (sqrt 3))])))
+
+; Berechnet die Länge einer Waage
+(: scale-length (image image real -> real))
+(check-expect (scale-length (circle 1 "solid" "red") (circle 2 "solid" "red") 1) 7)
+(check-expect (scale-length (rectangle 2 1 "solid" "red") (circle 2 "solid" "red") 1) 7)
+(define scale-length
+  (lambda (img1 img2 w2)
+    (+(+(image-width img1) (image-width img2)) w2)))
+ 
+; Zeichnet aus einer shape ein Bild(Kreis, Dreieck, Rechteck).
+(: draw-shape (shape -> image))
+(define draw-shape
+  (lambda(s)
+    (cond
+      [(c_circle? s) (circle (c_circle-radius s) "solid" (c_circle-color s))]
+      [(c_rectangle? s) (rectangle (c_rectangle-length s) (c_rectangle-height s) "solid" (c_rectangle-color s))]
+      [(c_triangle? s) (triangle (c_triangle-length s)  "solid" (c_triangle-color s))])))
+
+; Zeichnet für zwei shapes eine Waage.
+(: draw-scale (shape shape -> image))
+(define draw-scale
+  (lambda (s1 s2)
+    (rotate (shape-area s1) (shape-area s2)  (rotation-angle  (above(beside/align "bottom"  (draw-shape s1) (empty-scene 40 0) (draw-shape s2)) ( scale-length  (draw-shape s1) (draw-shape s2)) ( line 0 "black"))))))
+(draw-scale (circle 40 "solid" "red") (circle 40 "solid" "blue"))
+
+; Berechnet den Rotationswinkel für zwei Flächen.
+(: rotation-angle (real real -> real))
+(check-expect (rotation-angle 1 1) 0)
+(check-expect (rotation-angle 2 1) 45)
+(check-expect (rotation-angle 1 2) -45)
+(define rotation-angle
+  (lambda (A1 A2)
+    (* 90 (if(> A1 A2) (- 1 (/ A2  A1)) (+ -1 (/ A1  A2))))))
