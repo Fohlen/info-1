fc2d67b658d3761dccf1e4307361e0f18a42615d
Lennard Berger <lennard@berger.work>
2016-11-30 10:27:53 +0100

    Fix syntax and delete teachpack geocoder from A1

--- Blatt06/A1-let.rkt
+++ Blatt06/A1-let.rkt
@@ -1,16 +1,16 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname A1-let) (read-case-sensitive #f) (teachpacks ((lib "geocoder.rkt" "installed-teachpacks"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "geocoder.rkt" "installed-teachpacks")))))
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname A1-let) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Ohne let
 (define f
-  (lambda (x)
-    (lambda (r)
-      (i r (h r)))(r (g x))))
+  (lambda (x r)
+    ((lambda (r)
+      (i r (h r)))(r (g x)))))
 
 ; Dies ist gültig, weil let nur eine vereinfachte Schreibweise der Lambda-Abstraktion ist.
 	
 ;  Mit let
-(define f
+(define f_new
   (lambda (x)
     (let ((r (g x)))
       (i r
